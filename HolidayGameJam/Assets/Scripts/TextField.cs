da22a1ea63667a949c5b90792eed5ebd6d2af97a
Andrew Ogden <aogden@zynga.com>
2013-11-24 18:15:10 -0800

    Sprite, text label and text field in main scene

---
+++ HolidayGameJam/Assets/Scripts/TextField.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextField : MonoBehaviour {
+
+	public string InputString = "...";
+	void OnGUI() {
+		Vector3 screenPos = Camera.main.WorldToScreenPoint (transform.position);
+		InputString = GUI.TextField(new Rect(screenPos.x,screenPos.y, 300, 40), InputString, 25);
+	} 
+}
