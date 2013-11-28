3db0aa2fb29a935cfa38fcc3aab1146960b515b3
Blade Olson <bladeolson@gmail.com>
2013-11-27 22:00:35 -0800

    Test Function by Blade to get oriented in Unity

---
+++ HolidayGameJam/Assets/Scripts/BladeTestFunction.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BladeTestFunction : MonoBehaviour {
+
+	public TextField Blade;
+
+	private GameObject picture;
+	private Transform pictureTransform;
+	private Vector3 position;
+
+	// Use this for initialization
+	void Start () {
+		Blade = GameObject.Find("GameObject").GetComponent<TextField>();
+		picture = GameObject.Find ("New Sprite");
+		pictureTransform = picture.transform;
+		position = pictureTransform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Blade.InputString == "Booyah")
+		{
+			Vector3 tempvect = new Vector3(pictureTransform.position.x, pictureTransform.position.y+20.0f, pictureTransform.position.z);
+			position = tempvect;
+			Debug.Log("Hello");
+		}
+		else
+		{
+			//Debug.Log("Nope");
+		}
+	}
+}
